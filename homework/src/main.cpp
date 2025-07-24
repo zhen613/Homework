3b754637bfc4b3cdca37b78ce1774877cb4a8b09
zhen613 <kataspace01@gmail.com>
2025-07-24 19:07:14 +0800

    Update main.cpp

--- homework/src/main.cpp
+++ homework/src/main.cpp
@@ -1,1 +1,18 @@
+#include iostream
+using namespace std;
+int Ackerman(int m, int n) {
+    if (m == 0) {
+        return n + 1;
+    } else if (m > 0 && n == 0) {
+        return Ackerman(m - 1, 1);
+    } else if (m > 0 && n > 0) {
+        return Ackerman(m - 1, Ackerman(m, n - 1));
+    }
+}
 
+int main() {
+    int result;
+    result = Ackerman(2, 3);
+    cout << "Ackerman(2, 3) = " << result << endl;
+    return 0;
+}//遞迴
